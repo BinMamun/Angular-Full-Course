6cdbdd695a607a99d239b093eca786f95f33a038
Abdullah Bin Mamun <binbean44@gmail.com>
2023-12-25 21:02:38 +0600

    Parent to Child relationship and data binding

--- create-new-project/src/app/post/post.component.ts
+++ create-new-project/src/app/post/post.component.ts
@@ -1,9 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { PostListComponent } from '../post-list/post-list.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    PostListComponent
+  ],
   templateUrl: './post.component.html',
   styleUrl: './post.component.css'
 })
@@ -11,6 +16,7 @@ export class PostComponent implements OnInit {
 
   title: string = "List of post";
   @Input() fromParent!: string;
+  postMessage: string = "Message from post";
   ngOnInit(): void {
     console.log("From onInit");
   }
