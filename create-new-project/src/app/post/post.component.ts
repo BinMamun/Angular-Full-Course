5b74efee1a766f20f2d9db9a0ac10a173562f296
Abdullah Bin Mamun <binbean44@gmail.com>
2023-12-25 19:17:29 +0600

    OnInit and Data binding

--- create-new-project/src/app/post/post.component.ts
+++ create-new-project/src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
   templateUrl: './post.component.html',
   styleUrl: './post.component.css'
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
 
+  title: string = "List of post";
+  ngOnInit(): void {
+    console.log("From onInit");
+  }
 }
